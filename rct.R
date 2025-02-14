9fdcb9f9f9a4013eed6dd17cb99e50886fe1da42
Ben Domingue <ben.domingue@gmail.com>
2025-02-14 11:20:56 -0800

    rct

--- rct.R
+++ rct.R
@@ -22,13 +22,16 @@ plot(NULL,xlim=c(1,4),ylim=0:1,xlab="N",ylab="Proportion of times that I reject 
 axis(side=1,at=1:4,labels=10^(1:4))
 abline(h=.8,col='gray')
 cols<-colorRampPalette(c("blue", "red"))( length(L))
+tab<-list()
 for (i in 1:length(L)) {
     x<-L[[i]]
     m0<-loess(x[,2]<.05 ~ N)
     lines(N,m0$fitted,lty=1,col=cols[i])
     ii<-which.min(abs(m0$fitted-.8))
     iii<-which.min(abs(m0$fitted-.9))
-    print(c(names(L)[i],round(10^N[ii]),m0$fitted[ii],round(10^N[iii]),m0$fitted[iii]))
+    tab[[i]]<-c(names(L)[i],round(10^N[ii]),m0$fitted[ii],round(10^N[iii]),m0$fitted[iii])
 }
 ii<-rev(c(1,length(L)))
 legend("topleft",bty='n',legend=names(L)[ii],fill=cols[ii],title="Glass's delta")
+
+do.call("rbind",tab))
